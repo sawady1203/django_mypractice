1a18c8f477b6a50b6c86aa06c8c1574d82521e72
masa1203 <msys.sawada@gmail.com>
2019-12-14 19:31:27 +0900

    できた

--- django-todo/todo_app/views.py
+++ django-todo/todo_app/views.py
@@ -10,12 +10,13 @@ def index(request):
     if request.method == 'GET':
         all_items = List.objects.all()  # テーブルのアイテムを抜き出す.
         params = {
-            'all_items': all_items
+            'all_items': all_items,
         }
         return render(request, 'todo_app/index.html', params)
 
 
 def add(request):
+    print(request.method)
     if request.method == 'POST':
         form = ListForm(request.POST or None)
 
@@ -29,23 +30,52 @@ def add(request):
             messages.success(request, ('Item has been added to list!'))
             return render(request, 'todo_app/index.html', params)
         else:
-            messages.warning(requests, ('Validation faults!'))
+            messages.warning(request, ('Validation faults!'))
             print('Validation faults!')
             print(form.errors)
-            all_items = List.objects.all()
-            params = {
-                'all_items': all_items
-            }
-            return render(request, 'todo_app/index.html', params)
+            return redirect('todo_app:index')
+    else:
+        return redirect('todo_app:index')
+
 
 def delete(request, list_id):
-    pass
+    item = List.objects.get(id=list_id)
+    item.delete()  # 消去
+    messages.success(request, ('Item Has Been Deleted!'))
+    return redirect('todo_app:index')
+
 
 def cross_off(request, list_id):
-    pass
+    item = List.objects.get(id=list_id)
+    item.completed = True
+    item.save()
+    return redirect('todo_app:index')
+
 
 def uncross(request, list_id):
-    pass
+    item = List.objects.get(id=list_id)
+    item.completed = False
+    item.save()
+    return redirect('todo_app:index')
+
 
 def edit(request, list_id):
-    pass
+    if request.method == 'GET':
+        item = List.objects.get(id=list_id)
+        params = {
+            'item': item,
+        }
+        return render(request, 'todo_app/edit.html', params)
+    else:
+        item = List.objects.get(id=list_id)
+        form = ListForm(request.POST or None, instance=item)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Item has Been Edited!'))
+            return redirect('todo_app:index')
+        else:
+            messages.warning(request, ('validation faults!'))
+            print('edit validation faults!')
+            print(form.errors)
+            return redirect('todo_app:index')
