f3e3dbee7da1ee9107cd03218f60979be57a273a
masa1203 <msys.sawada@gmail.com>
2020-03-19 17:53:27 +0900

    progress: channel layer adding

--- django-channles-tutorial/config/routing.py
+++ django-channles-tutorial/config/routing.py
@@ -1,7 +1,13 @@
 # config/routing.py
-
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
 application = ProtocolTypeRouter({
-    # Empty for now (http->django views is added by default)
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
